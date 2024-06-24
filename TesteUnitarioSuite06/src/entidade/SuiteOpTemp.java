07b21ac50fd2070229e38e33adb2f067cbaf35a0
CarolinaSilva <142807604+CarolinaSilva22@users.noreply.github.com>
2024-06-24 19:27:34 -0300

    Add files via upload

---
+++ TesteUnitarioSuite06/src/entidade/SuiteOpTemp.java
@@ -0,0 +1,11 @@
+package entidade;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ OperacoesTeste.class, TemperaturaTeste.class })
+public class SuiteOpTemp {
+
+}
