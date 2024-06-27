879c2fa4452bee531689a7cf22a5021ccd16576e
CarolinaSilva <142807604+CarolinaSilva22@users.noreply.github.com>
2024-06-27 04:23:08 -0800

    Update TesteEquacaoSegundoGrau.java

--- TesteUnitario04/src/testes/TesteEquacaoSegundoGrau.java
+++ TesteUnitario04/src/testes/TesteEquacaoSegundoGrau.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import entidade.EquacaoSegundoGrau;
+import main.EquacaoSegundoGrau;
 
 public class TesteEquacaoSegundoGrau {
 	    EquacaoSegundoGrau servico;
